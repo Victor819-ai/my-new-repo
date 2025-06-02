f1a3eacf63fb306c88043c35db7d3ca69cd86631
Victor819-ai <loyner209@gmail.com>
2025-06-01 19:03:25 -0500

    update test3

--- test3.py
+++ test3.py
@@ -1,3 +1,3 @@
 import os
 
-print(f'Mi projecto desde de sensacional GitHub {os.getcwd()}')
+print(f'Mi projecto desde de sensacional GitHub es la priemra vez que los uso desde VS Code a GitHub {os.getcwd()}')
