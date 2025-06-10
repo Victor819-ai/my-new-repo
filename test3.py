f8e484ddcc1c4a3a7843252fbb0b4749f134392e
Victor819-ai <loyner209@gmail.com>
2025-06-09 23:17:55 -0500

    Actualiza app, README y notebook

--- test3.py
+++ test3.py
@@ -1,3 +1,14 @@
-import os
+import pandas as pd
+import plotly.express as px
 
-print(f'Mi projecto desde de sensacional GitHub es la priemra vez que los uso desde VS Code a GitHub {os.getcwd()}')
+# Leer los datos
+car_data = pd.read_csv('vehicles_us.csv')  # asegúrate que el archivo está en la misma carpeta
+
+# Crear histograma
+fig = px.histogram(car_data, x='odometer', title='Distribución de Kilometraje (odometer)')
+fig.show()
+
+
+# Crear gráfico de dispersión
+fig2 = px.scatter(car_data, x='odometer', y='price', title='Kilometraje vs Precio')
+fig2.show()
